5c3067432553899106f5e3b77c9dc0e235f49b97
Santiago Cante <114036508+CanteSantiago@users.noreply.github.com>
2023-11-27 21:56:16 -0500

    vid361-authservice
    comit error

--- AuthApp/src/app/app.module.ts
+++ AuthApp/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent,
@@ -11,8 +12,8 @@ import { AppRoutingModule } from './app-routing.module';
   ],
   imports: [
     BrowserModule,
-AppRoutingModule
-
+AppRoutingModule,
+HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
