9cfe50fb4df83aac26be8e3beef64f115849256c
Santiago Cante <114036508+CanteSantiago@users.noreply.github.com>
2023-11-27 20:59:08 -0500

    vid360-Formulacion de inicio de sesion

--- AuthApp/src/app/auth/pages/login-Pages/login-Pages.component.ts
+++ AuthApp/src/app/auth/pages/login-Pages/login-Pages.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login-Pages',
   templateUrl: './login-Pages.component.html',
   styleUrls: ['./login-Pages.component.css']
 })
-export class LoginPagesComponent implements OnInit {
+export class LoginPagesComponent   {
 
-  constructor() { }
+  private fb   = inject( FormBuilder );
 
-  ngOnInit() {
-  }
+
+  public myForm: FormGroup = this.fb.group({
+    email:    ['', [ Validators.required, Validators.email ]],
+    password: [' ', [ Validators.required, Validators.minLength(6) ]],
+  });
+
+login(){
+  console.log(this.myForm.value);
+
+}
 
 }
